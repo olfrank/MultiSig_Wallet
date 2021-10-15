a265586556b35606a98b81d2aac607c84c8e6097
Oliver Francis <olliefrancis97@hotmail.co.uk>
2021-10-15 15:09:48 +0100

    setup

--- contracts/client/js/main.js
+++ contracts/client/js/main.js
@@ -1,1 +1,17 @@
 import data from '../../../build/contracts/Wallet.json';
+
+Moralis.initialize("s8j9R2cqiJCrGskhbJHYK6xIeFszQizQjvOa2evd"); // Application id from moralis
+Moralis.serverURL = "https://q0ird0onim8h.grandmoralis.com:2053/server"; //Server url from moralis
+
+export async function loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
